6971d6c5ec629cb7ac76468db34c71f7dd51dca1
SafeEHA <98215429+SafeEHA@users.noreply.github.com>
2022-04-06 18:46:50 +0100

    files for capstone project

---
+++ code.js
@@ -0,0 +1,45 @@
+if (window.CSS && CSS.supports("color", "var(--primary)")) {
+    var toggleColorMode = function toggleColorMode(e) {
+      // Switch to Light Mode
+      if (e.currentTarget.classList.contains("light--hidden")) {
+        // Sets the custom html attribute
+        document.documentElement.setAttribute("color-mode", "light"); // Sets the user's preference in local storage
+  
+        localStorage.setItem("color-mode", "light");
+        return;
+      }
+      /* Switch to Dark Mode
+      Sets the custom html attribute */
+      document.documentElement.setAttribute("color-mode", "dark"); // Sets the user's preference in local storage
+  
+      localStorage.setItem("color-mode", "dark");
+    }; // Get the buttons in the DOM
+  
+    var toggleColorButtons = document.querySelectorAll(".color-mode__btn"); // Set up event listeners
+  
+    toggleColorButtons.forEach(function(btn) {
+      btn.addEventListener("click", toggleColorMode);
+    });
+  } else {
+    // If the feature isn't supported, then we hide the toggle buttons
+    var btnContainer = document.querySelector(".color-mode__header");
+    btnContainer.style.display = "none";
+  }
+  var scrollToTop = document.getElementById("go-top");
+window.onscroll = () => {
+    scrollFunction()
+};
+
+let scrollFunction = () => {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        scrollToTop.style.display = "block";
+    } else {
+        scrollToTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+  
